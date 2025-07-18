496360ff8851d6da23b997d559d914356053f88a
Oshitha <>
2025-07-18 09:39:39 +0530

    add git ignore

---
+++ app.config.ts
@@ -0,0 +1,114 @@
+import { ConfigContext, ExpoConfig } from "expo/config";
+
+// Replace these with your EAS project ID and project slug.
+// You can find them at https://expo.dev/accounts/[account]/projects/[project].
+const EAS_PROJECT_ID = "6eadc39e-8093-4dee-ac37-dd03674ff225";
+const PROJECT_SLUG = "recipes-app";
+const OWNER = "oshitha";
+
+// App production config
+const APP_NAME = "App Name";
+const BUNDLE_IDENTIFIER = "com.company.appname";
+const PACKAGE_NAME = "com.company.appname";
+const ICON = "./assets/images/icons/iOS-Prod.png";
+const ADAPTIVE_ICON = "./assets/images/icons/Android-Prod.png";
+const SCHEME = "app-scheme";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  console.log("⚙️ Building app for environment:", process.env.APP_ENV);
+  const { name, bundleIdentifier, icon, adaptiveIcon, packageName, scheme } =
+    getDynamicAppConfig(
+      (process.env.APP_ENV as "development" | "preview" | "production") ||
+        "development"
+    );
+
+  return {
+    ...config,
+    name: name,
+    version: "1.0.0",
+    slug: PROJECT_SLUG, // Must be consistent across all environments.
+    orientation: "portrait",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    icon: icon,
+    scheme: scheme,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: bundleIdentifier,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: adaptiveIcon,
+        backgroundColor: "#ffffff",
+      },
+      package: packageName,
+    },
+    updates: {
+      url: `https://u.expo.dev/${EAS_PROJECT_ID}`,
+    },
+    runtimeVersion: "1.0.0",
+    extra: {
+      eas: {
+        projectId: EAS_PROJECT_ID,
+      },
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    owner: OWNER,
+  };
+};
+
+// Dynamically configure the app based on the environment.
+// Update these placeholders with your actual values.
+export const getDynamicAppConfig = (
+  environment: "development" | "preview" | "production"
+) => {
+  if (environment === "production") {
+    return {
+      name: APP_NAME,
+      bundleIdentifier: BUNDLE_IDENTIFIER,
+      packageName: PACKAGE_NAME,
+      icon: ICON,
+      adaptiveIcon: ADAPTIVE_ICON,
+      scheme: SCHEME,
+    };
+  }
+
+  if (environment === "preview") {
+    return {
+      name: `${APP_NAME} Preview`,
+      bundleIdentifier: `${BUNDLE_IDENTIFIER}.preview`,
+      packageName: `${PACKAGE_NAME}.preview`,
+      icon: "./assets/images/icons/iOS-Prev.png",
+      adaptiveIcon: "./assets/images/icons/Android-Prev.png",
+      scheme: `${SCHEME}-prev`,
+    };
+  }
+
+  return {
+    name: `${APP_NAME} Development`,
+    bundleIdentifier: `${BUNDLE_IDENTIFIER}.dev`,
+    packageName: `${PACKAGE_NAME}.dev`,
+    icon: "./assets/images/icons/iOS-Dev.png",
+    adaptiveIcon: "./assets/images/icons/Android-Dev.png",
+    scheme: `${SCHEME}-dev`,
+  };
+};
